89d21dcd7d2e4c35aae7ef3c1b0945e1f0f085f2
Matt Krueger <mkruege2@gmail.com>
2018-04-18 13:14:19 -0500

    Time reporting

--- index.js
+++ index.js
@@ -2,43 +2,51 @@
 
 const co = require('co');
 
+let time = (()=>{
+  let t = process.hrtime();
+
+  return ()=>{
+    let tt = process.hrtime(t);
+    return `${tt[0]}:${Math.floor(tt[1]/1000000)}`;
+  };
+})();
+
 function*  test(){
   console.log('starting')
   let a = new Promise((r,j)=>{
     setTimeout(()=>{
-      console.log('a done');
+      console.log('%s a done', time());
       r('woot')
     }, 1000);
   });
 
   let b = new Promise((r, j)=>{
     setTimeout(()=>{
-      console.log('b done')
+      console.log('%s b done', time())
       r('ya')
     }, 5000)
   });
 
   let c = new Promise((r,j)=> {
     setTimeout(()=> {
-      console.log('c done')
+      console.log('%s c done', time())
       r('c done')
     }, 1000);
   });
 
   yield [a,b];
-  console.log('yield a-b done')
-  //
+  console.log('%s yield a-b done', time())
+
   yield b;
-  console.log('yield b done');
+  console.log('%s yield b done', time());
 
   yield c;
-  console.log('yield c done');
-
-  yield Promise.resolve('all done');
+  console.log('%s yield c done', time());
 }
 
-let d = co(test).then(dd=>{
-  console.log('we done')
-});
+let d = co(test);
 
+d.then(()=>{
+  console.log('%s all done', time());
+});
 
