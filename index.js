c546c2356a3fe28ab4009c3c10204062d16aa6a3
Matt Krueger <mkruege2@gmail.com>
2018-04-18 13:28:42 -0500

    Cleanup + nested promis

--- index.js
+++ index.js
@@ -13,38 +13,56 @@ let time = (()=>{
 
 function*  test(){
   console.log('starting')
-  let a = new Promise((r,j)=>{
+  let a = new Promise((r, x)=>{
     setTimeout(()=>{
       console.log('%s a done', time());
-      r('woot')
+      r()
     }, 1000);
   });
 
-  let b = new Promise((r, j)=>{
+  let b = new Promise((r, x)=>{
     setTimeout(()=>{
-      console.log('%s b done', time())
-      r('ya')
+      console.log('%s b done', time());
+      r()
     }, 5000)
   });
 
-  let c = new Promise((r,j)=> {
+  let c = new Promise((r, x)=> {
     setTimeout(()=> {
-      console.log('%s c done', time())
-      r('c done')
+      console.log('%s c done', time());
+      r()
     }, 1000);
   });
 
+  let nestedD1 = new Promise((r, x)=>{
+    setTimeout(()=>{
+      console.log('%s d1 done', time());
+      r();
+    }, 7000);
+  });
+
+  let d = new Promise((r, x)=>{
+    setTimeout(()=>{
+      console.log('%s d done', time());
+      r(nestedD1);
+    }, 6000);
+  });
+
   yield [a,b];
-  console.log('%s yield a-b done', time())
+  console.log('%s yield a-b done', time());
 
   yield b;
   console.log('%s yield b done', time());
 
   yield c;
   console.log('%s yield c done', time());
+
+  yield d;
+  console.log('%s yield d done', time());
 }
 
 let d = co(test);
+
 
 d.then(()=>{
   console.log('%s all done', time());
